064afc3a3fb0eb0f1e8ba51d362f8870c8890609
narenderreddygopu <narendergopu25@gmail.com>
2026-01-31 15:13:25 -0600

    final day4

--- Agentic_Course/test_data/data_read.py
+++ Agentic_Course/test_data/data_read.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Path to your file
-file_path = r"/mnt/c/Users/gopur/OneDrive/Documents/Agentic_ai/Agentic_Course/data.csv"
+file_path = r"/mnt/c/Users/gopur/OneDrive/Documents/Agentic_ai/Agentic_Course/test_data/data.csv"
 
 # Read CSV
 df = pd.read_csv(file_path)
