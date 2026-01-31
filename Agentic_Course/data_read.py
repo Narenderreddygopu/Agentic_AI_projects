eb87dd03250c72619e5a713446094eb9baf9d65f
narenderreddygopu <narendergopu25@gmail.com>
2026-01-31 13:14:50 -0600

    read

---
+++ Agentic_Course/data_read.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Path to your file
+file_path = r"/mnt/c/Users/gopur/OneDrive/Documents/Agentic_ai/Agentic_Course/data.csv"
+
+# Read CSV
+df = pd.read_csv(file_path)
+
+# Show first 5 rows
+print(df.head())
