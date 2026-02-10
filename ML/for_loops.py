464d620a5602a159524094b9a7a4e561ecd4a2c4
narenderreddygopu <narendergopu25@gmail.com>
2026-02-09 22:38:45 -0600

    ML 1

---
+++ ML/for_loops.py
@@ -0,0 +1,52 @@
+"""
+
+
+
+Types of ML (high level)
+
+1️⃣ Supervised Learning
+
+Learns from labeled data
+
+Examples: prediction, classification
+
+2️⃣ Unsupervised Learning
+
+Finds hidden structure
+
+Examples: clustering, grouping
+
+3️⃣ Reinforcement Learning
+
+Learns by trial & reward
+
+Examples: robotics, games  give exmaples
+for i in range(1, 11):
+    print(i)
+
+for i in range(1, 11):
+    print(i**2)
+
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print("a")
+
+
+for i in range(1, 3):
+    for j in range(1, 3):
+        for k in range(1, 4):
+            #print(k)
+            print(f"i={i}, j={j}, k={k}")
+
+
+
+for i in range(1, 11):
+    for j in range(20, 81):
+        for k in range(101, 351):
+            print(k)
+"""
+for i in range(1, 5):
+    for j in range(1, 3):
+        print("*", end=" ")
+    print()
